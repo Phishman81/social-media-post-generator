00aa2ffaa08ee2a346a3702be1b1fc5bafdabb9f
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-17 01:31:40 +0200

    Update test-linkedin.py

--- test-linkedin.py
+++ test-linkedin.py
@@ -4,13 +4,16 @@ import openai
 # Streamlit application
 def main():
 
-    st.title('LinkedIn Post Generator with GPT-4')
+    st.title('Social Media Post Generator with GPT-4')
     
     # Input field for user's OpenAI API key
     openai_api_key = st.text_input('Enter your OpenAI API key')
     
-    # Input field for the desired LinkedIn post topic
-    post_topic = st.text_input('What do you want a LinkedIn post about?', max_chars=400)
+    # Dropdown for selecting the social media platform
+    platform = st.selectbox('Choose your social media platform', ['LinkedIn', 'Instagram', 'Facebook', 'YouTube', 'TikTok', 'Snapchat', 'Google Profile Page'])
+    
+    # Input field for the desired post topic
+    post_topic = st.text_input(f'What do you want your {platform} post to be about?', max_chars=400)
     
     if st.button('Generate Post'):
         if openai_api_key and post_topic:
@@ -18,7 +21,7 @@ def main():
             openai.api_key = openai_api_key
             
             # Create the GPT-4 prompt
-            prompt_text = f"Act as a highly experienced LinkedIn professional and craft a very compelling and interesting LinkedIn post out of the following information: {post_topic}"
+            prompt_text = f"Act as a highly experienced {platform} professional and craft a very compelling and interesting {platform} post out of the following information: {post_topic}"
             
             # Generate the post using GPT-4
             try:
@@ -34,7 +37,7 @@ def main():
             except Exception as e:
                 st.write(str(e))
         else:
-            st.write('Please enter both your OpenAI API key and the LinkedIn post topic.')
+            st.write('Please enter both your OpenAI API key and the topic for your post.')
         
 if __name__ == "__main__":
     main()
