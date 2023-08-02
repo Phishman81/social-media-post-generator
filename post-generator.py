119d91c2e3bb1cb3421a370ce100d89cf7c252db
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-08-02 11:15:36 +0200

    Update post-generator.py-new image

--- post-generator.py
+++ post-generator.py
@@ -9,7 +9,7 @@ def add_bg_from_url():
         f"""
         <style>
         .stApp {{
-            background-image: url("https://cdn.midjourney.com/6a6876f7-0947-41b7-9e15-aeb0988ed0cc/0_0.webp");
+            background-image: url("https://cdn.midjourney.com/2acbbb31-62ba-4d99-91d9-7b2b43764777/0_0.webp");
             background-attachment: fixed;
             background-size: cover;
         }}
